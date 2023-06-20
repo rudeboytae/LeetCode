1fc88dc3bd5f23403d32418cfc98d9af846c5943
Jeong tae nyeong <137030888+rudeboytae@users.noreply.github.com>
2023-06-20 09:03:07 +0900

    Time: 76 ms (18.60%), Space: 16.4 MB (29.79%) - LeetHub

---
+++ 2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.py
@@ -0,0 +1,12 @@
+class Solution:
+    def finalValueAfterOperations(self, operations: List[str]) -> int:
+        
+        count = 0
+    
+        for i in operations:
+            if(i == "++X" or i == "X++"):
+                count += 1
+            elif(i == "--X" or i == "X--"):
+                count -= 1
+            
+        return count
